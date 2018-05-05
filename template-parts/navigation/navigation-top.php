e2efad70aae51a0f5fe606211fde47211ccb2281
Daniel Dawson <daniel.jon.dawson@gmail.com>
2018-05-05 01:56:54 +0000

    First commit

---
+++ template-parts/navigation/navigation-top.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Displays top navigation
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.2
+ */
+
+?>
+<nav id="site-navigation" class="main-navigation" role="navigation">
+<?php
+var_dump(wp_nav_menu());
+?>
+
+</nav><!-- #site-navigation -->
